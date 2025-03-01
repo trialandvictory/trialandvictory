b549a4403afce0dcfa5999e60716ba70d4690d1c
Robin van der Noord <robinvandernoord@gmail.com>
2025-03-01 14:14:32 +0100

    fix(bun): switch dev and prd dependencies around, remove tailwind

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite"
 import laravel from "laravel-vite-plugin"
-import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
     plugins: [
@@ -8,18 +7,5 @@ export default defineConfig({
             input: ["resources/css/app.scss", "resources/js/app.ts"],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 })
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/scss/app.scss', 'resources/ts/app.ts'],
-//             refresh: true,
-//         }),
-//     ],
-// });
