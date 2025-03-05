95654b1743d9e36c927f660f972b45c166dee624
VictorZwart <victor.zwart@ziggo.nl>
2025-03-05 16:12:02 +0100

    docs(routes): added the missing pages

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PageController;
 Route::controller(PageController::class)->group(function () {
     Route::get("/", "index")->name('index');
     Route::get('/about-us', "about_us")->name('about_us');
+    // fixme: add the following pages: Wat doen we / Tarieven / Portfolio / Contact
 
     // fixme: middleware voor local-only proberen:
     Route::get('/elements', "elements")->name('elements');
